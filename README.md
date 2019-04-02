93377675b3701a3fcebe4fdd1ca5620510e2b6d7
William Notowidagdo <wnotowidagdo@gmail.com>
2019-04-02 20:36:48 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Opsworks WordPress Demo
+
+This code is a one-line recipe that displays a simple message in the log when the recipe runs:
+
+    ********** Hello, World! **********
---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'opsworks_wordpress_demo'
+maintainer 'John Doe'
+maintainer_email 'john@example.com'
+license 'MIT'
+version '0.1.0'
+chef_version '>= 12.0'
+supports 'ubuntu'
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+issues_url 'https://github.com/williamn/opsworks_wordpress_demo/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+source_url 'https://github.com/williamn/opsworks_wordpress_demo'
