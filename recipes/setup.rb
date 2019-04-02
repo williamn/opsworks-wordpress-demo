868906aaaffec6e740fc6e0bf9d0b1a79661ebc0
William Notowidagdo <wnotowidagdo@gmail.com>
2019-04-02 21:39:35 +0700

    update setup recipe: change info.php owner to root

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -3,7 +3,7 @@ apt_package ['apache2', 'mysql-server', 'php', 'libapache2-mod-php', 'php-mysql'
 cookbook_file "Copy info.php" do
   group "root"
   mode "0755"
-  owner "ec2-user"
+  owner "root"
   path "/var/www/html/info.php"
   source "info.php"
 end
