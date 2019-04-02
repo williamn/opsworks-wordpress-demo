0a1cdbacf2001ab2d050435d415971331e46f4ac
William Notowidagdo <wnotowidagdo@gmail.com>
2019-04-02 21:56:43 +0700

    setup recipe: change mysql password update command

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,11 +10,12 @@ end
 
 execute "Secure MySQL installation" do
   command "mysql --user=root <<_EOF_
-UPDATE mysql.user SET Password=PASSWORD('#{node['db_root_password']}') WHERE User='root';
+UPDATE USER SET authentication_string=password('#{node['db_root_password']}') where user='root';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 _EOF_"
+  command "echo -e "\n\nmysecret\nmysecret\n\n\nn\n\n " | mysql_secure_installation 2>/dev/null"
 end
