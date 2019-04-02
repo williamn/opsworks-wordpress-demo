6cc9e806ac93e82d63717b31f0213020fa0c2ba6
William Notowidagdo <wnotowidagdo@gmail.com>
2019-04-02 21:49:04 +0700

    add command to secure mysql

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -7,3 +7,14 @@ cookbook_file "Copy info.php" do
   path "/var/www/html/info.php"
   source "info.php"
 end
+
+execute "Secure MySQL installation" do
+  command "mysql --user=root <<_EOF_
+UPDATE mysql.user SET Password=PASSWORD('#{node['db_root_password']}') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+_EOF_"
+end
