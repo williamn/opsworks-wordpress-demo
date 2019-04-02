828a96c21f26454dded29213b3ffd6e80ff70c04
William Notowidagdo <wnotowidagdo@gmail.com>
2019-04-02 21:36:33 +0700

    copy file info.php

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,1 +1,9 @@
 apt_package ['apache2', 'mysql-server', 'php', 'libapache2-mod-php', 'php-mysql']
+
+cookbook_file "Copy info.php" do
+  group "root"
+  mode "0755"
+  owner "ec2-user"
+  path "/var/www/html/info.php"
+  source "info.php"
+end
