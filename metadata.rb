be0b6390e7f1b767d78e905e8067dfe3caad73b8
William Notowidagdo <wnotowidagdo@gmail.com>
2019-04-03 23:54:18 +0700

    metada: change support to amazon

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'john@example.com'
 license 'MIT'
 version '0.1.0'
 chef_version '>= 12.0'
-supports 'ubuntu'
+supports 'amazon'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
