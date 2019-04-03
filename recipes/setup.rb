e32b8bbcb00490619308c0ff95f2818a12683759
William Notowidagdo <wnotowidagdo@gmail.com>
2019-04-04 00:08:55 +0700

    satisfy cookstyle complaints

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,27 +10,26 @@ execute 'Start Apache web server' do
   command 'sudo systemctl enable httpd'
 end
 
-execute 'Set file permissions' do 
+execute 'Set file permissions' do
   command 'sudo usermod -a -G apache ec2-user'
   command 'sudo chown -R ec2-user:apache /var/www'
   command 'sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;'
   command 'find /var/www -type f -exec sudo chmod 0664 {} \;'
 end
-
-cookbook_file "Copy info.php" do
-  group "root"
-  mode "0755"
-  owner "root"
-  path "/var/www/html/info.php"
-  source "info.php"
+cookbook_file 'Copy info.php' do
+  group 'root'
+  mode '0755'
+  owner 'root'
+  path '/var/www/html/info.php'
+  source 'info.php'
 end
 
-execute 'Start MariaDB server'
+execute 'Start MariaDB server' do
   command 'sudo systemctl start mariadb'
   command 'sudo systemctl enable mariadb'
 end
 
-execute "Secure MySQL installation" do
+execute 'Secure MySQL installation' do
   command "mysql --user=root <<_EOF_
 USE mysql;
 UPDATE user SET authentication_string=password('#{node['db_root_password']}') where user='root';
