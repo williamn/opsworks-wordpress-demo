bbfdc31a1d90714385bc16586bae6d873aa1bbbe
William Notowidagdo <wnotowidagdo@gmail.com>
2019-04-04 13:32:15 +0700

    fixd STDERR: ERROR 1064 (42000) at line 3: You have an error in your SQL syntax;

--- recipes/create_wordpress_database.rb
+++ recipes/create_wordpress_database.rb
@@ -1,6 +1,6 @@
 execute "mysql -u root -p#{node['db_root_password']} <<_EOF_
 CREATE USER '#{node['wordpress_db_user']}'@'localhost' IDENTIFIED BY '#{node['wordpress_db_password']}';
 CREATE DATABASE #{node['wordpress_db_name']};
-GRANT ALL PRIVILEGES ON '#{node['wordpress_db_name']}'.* TO '#{node['wordpress_db_user']}'@'localhost';
+GRANT ALL PRIVILEGES ON #{node['wordpress_db_name']}.* TO '#{node['wordpress_db_user']}'@'localhost';
 FLUSH PRIVILEGES;
 _EOF_"
